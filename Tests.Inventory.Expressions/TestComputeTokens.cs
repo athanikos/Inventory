42520df43ded545d7c0a91941adc79c5c3a6477e
Nikos Athanasakis <athanikos@gmail.com>
2024-06-13 20:55:40 +0300

    First xunit that runs with DI

--- Tests.Inventory.Expressions/TestComputeTokens.cs
+++ Tests.Inventory.Expressions/TestComputeTokens.cs
@@ -1,7 +1,10 @@
-﻿using Xunit.Microsoft.DependencyInjection.Abstracts;
-using Xunit.Abstractions;
-using MediatR;
+﻿using Expressions;
+using Inventory.Products.Contracts.Dto;
+using Inventory.Products.Dto;
 using Inventory.Products.Repositories;
+using MediatR;
+using Xunit.Abstractions;
+using Xunit.Microsoft.DependencyInjection.Abstracts;
 
 namespace Tests.Inventory.Expressions
 {
@@ -15,21 +18,36 @@ namespace Tests.Inventory.Expressions
         public EvaluatorTests(ITestOutputHelper testOutputHelper, TestFixture fixture) :
             base(testOutputHelper, fixture)
         {
-        
+
         }
 
         [Fact]
         public async void TestComputeTokens()
         {
 
-           
-                var mediator = _fixture.GetService<IMediator>(_testOutputHelper)!;
-                var repo = _fixture.GetService<IInventoryRepository>(_testOutputHelper)!;
 
+            var _mediator = _fixture.GetService<IMediator>(_testOutputHelper)!;
+            var _repo = _fixture.GetService<IInventoryRepository>(_testOutputHelper)!;
 
-                Assert.Equal(1, 1);
+            await _repo.AddInventoryAsync(new InventoryDto(Guid.NewGuid(), "CRYPTO"));
+            var metricId = (await _repo.AddMetricAsync(new MetricDto(Guid.NewGuid(), "", "QUANTITY", Guid.NewGuid()))).Id;
+
+            var prodDto = new ProductDto(Guid.NewGuid(),
+                                         "",
+                                         "ADA",
+                                         Guid.NewGuid(),
+                                         new List<ProductMetricDto>());
+
+            var productId = (await _repo.AddProductAsync(prodDto)).Id;
+            await _repo.AddOrEditProductMetric(new ProductMetricDto(productId, metricId, 1, DateTime.MinValue, "EUR", "ADA", "QUANTITY"));
+
+            Evaluator instance = new Evaluator(_mediator, "QUANTITY(ADA)");
+            string result = await instance.Execute();
+
+            Assert.Equal(1, 1);
 
         }
+
 
     }
 }
