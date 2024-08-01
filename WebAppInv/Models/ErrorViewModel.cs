17b9b86c91d665984a2aad620c46fa324593ac9c
nikosathanasakis <athanikos@gmail.com>
2024-08-01 17:14:26 +0300

    Remove unused folders

--- WebAppInv/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAppInv.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
